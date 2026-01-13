96e91fa22eb7ff6a636ade9c8b478337c70ce830
o7digital <101292703+o7digital@users.noreply.github.com>
2026-01-12 18:10:58 -0600

    Replace NO SELL label with NOT FOR SALE

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -66,7 +66,7 @@ const formatPrice = (price?: string | null, regular?: string | null) => {
       maximumFractionDigits: 2,
     })} USD`;
   }
-  return 'NO SELL';
+  return 'NOT FOR SALE';
 };
 
 const deriveDimensions = (
